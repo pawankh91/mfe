0576abd4fce036a92bb6046223331648c4db2cd4
Pawan K H <pkh@fastenal.com>
2023-10-16 11:46:30 +0530

    added dashboard

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,24 +1,11 @@
 import { mount } from 'auth/AuthenticationApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 
-export default ({ onSignIn }) => {
+export default () => {
     const ref = useRef(null);
-    const history = useHistory();
 
     useEffect(() => {
-        const { onParentNavigate } = mount(ref.current, {
-            onNavigate: ({ pathname: nextPathname }) => {
-                const { pathname } = history.location;
-
-                if (pathname !== nextPathname) {
-                    history.push(nextPathname);
-                }
-            },
-            onSignIn
-        });
-
-        history.listen(onParentNavigate);
+        mount(ref.current);
     }, []);
 
     return <div ref={ref} />
