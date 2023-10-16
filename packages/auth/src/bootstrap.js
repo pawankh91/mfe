f2447d3d344110e89d1a5c3fe345161e94e07013
Pawan K H <pkh@fastenal.com>
2023-10-16 10:55:52 +0530

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory }) => {
     const history = defaultHistory || createMemoryHistory();
 
     if (onNavigate) {
         history.listen(onNavigate);
     }
 
-    ReactDOM.render(<App history={history} />, el);
+    ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
     return {
         onParentNavigate({ pathname: nextPathname }) {
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 // If we are in development and in isolation
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
 
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
